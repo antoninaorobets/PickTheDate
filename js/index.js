37e19017166005efd0b0d4696984680360ec1bd1
antoninaorobets <pani.antonina.don@gmail.com>
2021-12-23 14:42:22 -0500

    add listemers to swich between items on nav bar

---
+++ js/index.js
@@ -0,0 +1,61 @@
+document.addEventListener('DOMContentLoaded', init)
+function init(event) {
+    const home = document.querySelector('#home')
+    home.addEventListener('click', displayHomeDiv)
+    const list = document.querySelector('#event-list')
+    list.addEventListener('click', displayListDiv)
+    const create = document.querySelector('#create-event')
+    create.addEventListener('click', displayCreateDiv)
+    tryDB() 
+
+}
+
+const mainDiv = document.querySelector('#main')
+
+
+function displayListDiv(event) {
+    mainDiv.innerHTML = ""
+    mainDiv.innerHTML =
+        '<h1>Current Events</h1>' +
+        '<ul>' +
+        `    <li data-id="1">Steph's party </li>` +
+        '  <li data-id="2">Vacation planing meating</li>' +
+        ' </ul>'
+    console.log(event)
+}
+
+function displayCreateDiv(event) {
+    mainDiv.innerHTML = ""
+    mainDiv.innerHTML =
+        '<h1>Create New Event</h1>' +
+        '<form action="#" id="new-event">' +
+        '<label id="event-name">Event name</label>' +
+        ' <input type="text" name="event-name" />' +
+        ' <label id="start-data">From</label>' +
+        '<input type="date" name="start-data" />' +
+        ' <label id="start-data">Till</label>' +
+        ' <input type="date" name="end-data" />' +
+        '<input type="submit" value="Submit" style="background-color: #F9AA33;">' +
+        ' </form>'
+
+    console.log(event)
+}
+function displayHomeDiv(event) {
+    mainDiv.innerHTML = ''
+    mainDiv.innerHTML =
+        '<h1>Pick the Dates</h1>' +
+        '<p>This web app helps friends to pick the best date to meet.</p>' +
+        '<button>Create event</button>'
+
+    console.log(event)
+}
+
+function tryDB() {
+
+
+    fetch('http://localhost:3000/events')
+        .then((response) => response.json())
+        .then((object) => console.log(object))
+        .catch((error) => console.error(error.message))
+
+}
