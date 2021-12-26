073fff5775688d70e4a3fd19e7117649d8016333
antoninaorobets <pani.antonina.don@gmail.com>
2021-12-26 13:05:15 -0500

    add table

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', init)
 function init(event) {
-    displayHome() 
+    displayHome()
     const home = document.querySelector('#home')
     home.addEventListener('click', displayHome)
     const weather = document.querySelector('#weather')
@@ -41,27 +41,27 @@ function displayList(event) {
         .then((response) => response.json())
         .then(createList)
         .catch((error) => console.error(error.message))
-        const list = document.createElement('div')
-        list.classList.add("list-group")
-        mainDiv.append(list)
-    function createList( data) {
+    const list = document.createElement('div')
+    list.classList.add("list-group")
+    mainDiv.append(list)
+    function createList(data) {
         for (let element of data) {
             const a = document.createElement('a')
-            a.dataset.eventId=element.id
-            a.textContent=element.title
-            a.href="#"
+            a.dataset.eventId = element.id
+            a.textContent = element.title
+            a.href = "#"
             a.classList.add("list-group-item")
             a.classList.add("list-group-item-action")
-            a.addEventListener('click',getEventData)
+            a.addEventListener('click', getEventData)
             list.append(a)
         }
     }
 }
 
 function getEventData(event) {
-    fetch('http://localhost:3000/events'+ `/${event.target.dataset.eventId}`,)
+    fetch('http://localhost:3000/events' + `/${event.target.dataset.eventId}`,)
         .then((response) => response.json())
-        .then(displayCurrentDiv)
+        .then(displayCurrentEvent)
         .catch((error) => console.error(error.message))
 }
 
@@ -70,24 +70,24 @@ function displayCreateDiv(event) {
     mainDiv.innerHTML = ""
     mainDiv.innerHTML =
         '<h1>Create New Event</h1>' +
-        
+
         '<form action="#" id="new-event" class="row g-3">' +
-     
-        '<div class="col-12">'+
+
+        '<div class="col-12">' +
         '<label id="event-name"  class="form-label">Event name</label>' +
         ' <input type="text" name="event-name" class="form-control"/> </div>' +
-        ' <div class="col-md-6">'+
+        ' <div class="col-md-6">' +
         ' <label id="start-data"   class="form-label">From date</label>' +
         '<input type="date" name="start-data"  class="form-control"/> </div>' +
-        ' <div class="col-md-6">'+
+        ' <div class="col-md-6">' +
         ' <label id="start-data"  class="form-label">Till date</label>' +
         ' <input type="date" name="end-data" class="form-control" /> </div>' +
 
         '<input type="submit" value="Submit" class="btn btn-primary" style="background-color: #F9AA33;">' +
-      
+
         ' </form>'
     const form = document.querySelector('form')
-    form.addEventListener('submit',createEvent)
+    form.addEventListener('submit', createEvent)
 }
 
 function createEvent(event) {
@@ -95,10 +95,10 @@ function createEvent(event) {
     const input = event.target.querySelector("input[name='event-name']").value
     let start = event.target.querySelector("input[name='start-data']").value
     const end = event.target.querySelector("input[name='end-data']").value
-    const interval = getNumberOfDays(start, end) 
-    const dateRange =[start]
+    const interval = getNumberOfDays(start, end)
+    const dateRange = [start]
 
-    for (let i=0;i<interval;i++) {
+    for (let i = 0; i < interval; i++) {
         let d = new Date(start)
         d.setDate(d.getDate() + 1)
         dateRange.push(d)
@@ -112,8 +112,8 @@ function createEvent(event) {
         "end": `${end}`,
         "days": dateRange,
     }
-   
-    fetch('http://localhost:3000/events',{
+
+    fetch('http://localhost:3000/events', {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
@@ -122,73 +122,71 @@ function createEvent(event) {
         body: JSON.stringify(formData),
     })
         .then((response) => response.json())
-        .then(displayCurrentDiv)
+        .then(displayCurrentEvent)
         .catch((error) => console.error(error.message))
 }
 
-function displayCurrentDiv(data){
-    mainDiv.innerHTML = "" 
+function displayCurrentEvent(data) {
+    console.log('current event data', data)
+    mainDiv.innerHTML = ""
     const title = document.createElement('h1')
     title.textContent = data.title
     mainDiv.append(title)
     const summary = document.createElement('div')
-    summary.innerHTML =  '<p>The most popular date for this event:' +
-           '<span>Wednesday, 1/18/2022</span> selected by 5 particpants from 10</p>'
+    summary.innerHTML = '<p>The most popular date for this event:' +
+        '<span>Wednesday, 1/18/2022</span> selected by 5 particpants from 10</p>'
     mainDiv.append(summary)
     const btn = createBtn("participate", "Participate")
-    btn.addEventListener('click', openAvailabilityForm)
+    btn.addEventListener('click', (event) => openAvailabilityForm(event, data))
     mainDiv.append(btn)
-
-    console.log('data' , data)
-
-    function openAvailabilityForm(event) {
-        
-        const form = document.createElement('form')
-        form.id = "availabiloty-form"
-       // form.dataset.eventId = data.id
-        form.addEventListener('submit',  (event) => updateEventSummary(event,data))
-        mainDiv.append(form)
-        const input = createInput("user-name", "Name")
-        form.append(input)
-        const div = document.createElement('div')
-       // div.id = "check-list"
-        form.append(div)
-        for (let day of data.days) {
-            const a = new Date(day);
-
-            const check  = createCheck(a.toString().slice(0,15))
-        div.append(check)
-        }
-
-        const submit = createSubmitBtn()
-        form.append(submit)
+    
+    const form = document.createElement('form')
+    form.id = "availabiloty-form"
+    mainDiv.append(form)
+    
+    const visualSection = document.createElement('section')
+    mainDiv.append(visualSection)
+    if ( Object.keys(data.particitants).length === 0 ) {
+    visualSection.textContent = 'Tut budet tablichka' 
+    }
+    else {
+        displayAvailabilityTable(data,visualSection)
     }
 }
 
-function updateEventSummary(event, eventData){
+function openAvailabilityForm(event, eventData){
+    const form = mainDiv.querySelector('#availabiloty-form')
+    form.addEventListener('submit', (event) => updateEventSummary(event, eventData))  //doean't work if move to displayCurrentEvent function
+    const input = createInput("user-name", "Name")
+    form.append(input)
+    const div = document.createElement('div')
+    form.append(div)
+    for (let day of eventData.days) {
+        const a = new Date(day);
+        const check = createCheck(a.toString().slice(0, 15))
+        div.append(check)
+    }
+    const submit = createSubmitBtn()
+    form.append(submit)
+}
+
+function updateEventSummary(event, eventData) {
     event.preventDefault()
     const userName = event.target.querySelector('input[name="user-name"]').value
-    const checkList =event.target.querySelectorAll(".form-check")
+    const checkList = event.target.querySelectorAll(".form-check")
     const availability = []
     checkList.forEach(check => availability.push(check.querySelector('input').checked))
     const form = mainDiv.querySelector('#availabiloty-form')
     mainDiv.removeChild(form)
     const id = eventData.id
 
-    
-    let obj ={}
-    obj[userName] = availability
-
     eventData.particitants[userName] = availability
     console.log(eventData.particitants)
-    const data = { 
+    const data = {
         particitants: eventData.particitants
     }
-
-
-
-
-    fetch('http://localhost:3000/events' + `/${id}`,{
+    // ?????????? How to ubpage without passin all object
+    fetch('http://localhost:3000/events' + `/${id}`, {
         method: "PATCH",
         headers: {
             "Content-Type": "application/json",
@@ -197,25 +195,61 @@ function updateEventSummary(event, eventData){
         body: JSON.stringify(data),
     })
         .then((response) => response.json())
-        .then(displayCurrentDiv)
+        .then(displayCurrentEvent)
         .catch((error) => console.error(error.message))
+}
+
+function displayAvailabilityTable(eventData, section) {
+    console.log('displayAvailabilityTable')
+    const tableTitle = createHeader('h4', 'Results')
+    section.append(tableTitle)
+    const table = document.createElement('table')
+    table.className = "table"
+    section.append(table)
+    const thead = document.createElement('thead')
+    table.append(thead)
+    const tr = document.createElement('tr')
+    thead.append(tr)
+
+    const numCol = eventData.days.length + 1 
+    const th = document.createElement('th')
+        th.scope ="col"
+        th.textContent = 'Paricipants'
+        tr.append(th)
+    for (let i = 1; i< numCol; i++ ) {
+        const d = new Date (eventData.days[i-1])
+        const th = document.createElement('th')
+        th.scope ="col"
+        th.textContent = d.toString().slice(8,13).replace(' ', '/')
+        tr.append(th)
+    // const thBtn = document.createElement('th')
+    // thBtn.scope ="col"
+    // thBtn.textContent = ''
+    // tr.append(thBtn)
+
+    }
+
+
+    const tbody = document.createElement('tbody')
+    teble.append(tbody)
+
 }
 
 
 // Create Elements functons
-function createHeader(type,text){
+function createHeader(type, text) {
     const header = document.createElement(`${type}`)
     header.textContent = text
     return header
 }
-function createParagraph(text){
+function createParagraph(text) {
     const paragraph = document.createElement(`p`)
     paragraph.textContent = text
     return paragraph
 }
 
-function createBtn(btnId, text){
-    const btn = document.createElement('button') 
+function createBtn(btnId, text) {
+    const btn = document.createElement('button')
     btn.textContent = text
     btn.id = btnId
     btn.classList.add("btn")
@@ -259,14 +293,14 @@ function createInput(inputName, lableText) {
     return div
 }
 
-function createSubmitBtn(){
-        const btn =  document.createElement('input')
-        btn.classList.add("btn")
-        btn.classList.add("btn-primary")
-        btn.type = "submit"
-        btn.value="Submit"   
+function createSubmitBtn() {
+    const btn = document.createElement('input')
+    btn.classList.add("btn")
+    btn.classList.add("btn-primary")
+    btn.type = "submit"
+    btn.value = "Submit"
     // style="background-color: #F9AA33;"
- return btn
+    return btn
 }
 
 //Aditinal calculations functions
@@ -285,12 +319,12 @@ function displayWeather(event) {
     const header = createHeader("h1", "Weather Forecast")
     mainDiv.append(header)
     const form = document.createElement('form')
-    form.className ="mb-3"
-    form.addEventListener('submit',requestWeatherData)
+    form.className = "mb-3"
+    form.addEventListener('submit', requestWeatherData)
     mainDiv.append(form)
     const input = createInput("zipcode", "City")
     form.append(input)
-    const submit  = createSubmitBtn()
+    const submit = createSubmitBtn()
     form.append(submit)
 
     // code to ask for location and dates befor request API  
@@ -301,18 +335,18 @@ function displayWeather(event) {
     function requestWeatherData(event) {
         event.preventDefault()
         mainDiv.removeChild(form)
-        const zipcode =event.target.querySelector("input[name='zipcode']").value.toString()
+        const zipcode = event.target.querySelector("input[name='zipcode']").value.toString()
         //convert name to zip??
-        requestWeatherAPI(zipcode).then((data)=> {
-             console.log(data.response[0].periods[0].maxTempC)
-             displayForecast(data)
+        requestWeatherAPI(zipcode).then((data) => {
+            console.log(data.response[0].periods[0].maxTempC)
+            displayForecast(data)
         })
-    
+
     }
 }
 
 
-function displayForecast(data){
+function displayForecast(data) {
     // const paragraph3 = createParagraph(``)
     // mainDiv.append(paragraph3)
     const table = document.createElement('table')
@@ -325,17 +359,17 @@ function displayForecast(data){
     const tr = document.createElement('tr')
     thead.append(tr)
     tr.innerHTML = '<th scope="col">Day</th><th scope="col">Img</th><th scope="col">Max</th> <th scope="col">Min</th>'
-    const days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
-    for (let day of data.response[0].periods){
+    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    for (let day of data.response[0].periods) {
         const tr = document.createElement('tr')
         tbody.append(tr)
-        const a = new Date(day.timestamp*1000);
+        const a = new Date(day.timestamp * 1000);
         const dayOfWeek = days[a.getDay()]
 
-        tr.innerHTML = `<td>${dayOfWeek}<br>${a.toString().slice(4,15)}</td><td>${day.weather}</td><td>${day.maxTempC} C</td><td>${day.minTempC} C</td>`
+        tr.innerHTML = `<td>${dayOfWeek}<br>${a.toString().slice(4, 15)}</td><td>${day.weather}</td><td>${day.maxTempC} C</td><td>${day.minTempC} C</td>`
         // <th scope="row">1</th>
-   
-  
+
+
         console.log(day.maxTempC)
     }
 }
@@ -343,8 +377,8 @@ function displayForecast(data){
 
 function requestWeatherAPI(zipcode) {
     // location, start,end
-  
-  return  fetch(`https://aerisweather1.p.rapidapi.com/forecasts/${zipcode}?from=2021-12-30&to=2022-01-19`, {
+
+    return fetch(`https://aerisweather1.p.rapidapi.com/forecasts/${zipcode}?from=2021-12-30&to=2022-01-19`, {
         "method": "GET",
         "headers": {
             "x-rapidapi-host": "aerisweather1.p.rapidapi.com",
